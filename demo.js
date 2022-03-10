68af3ea3934f4fd244285e848e130bc413eecf7c
SQB <sqb@beifendeMacBook-Pro.local>
2022-03-10 10:43:41 +0800

    demo

--- demo.js
+++ demo.js
@@ -1,4 +1,5 @@
 //  const cdn = 'https://statics.cdn.com/cdn/swiper/8.0.6/swiper-bundle.css'
  <link rel="stylesheet" href='https://statics.cdn.com/cdn/swiper/8.0.6/swiper-bundle.css11'></link>
+ <link rel="stylesheet" href='https://statics.cdn.com/cdn/swiper/8.0.6/swiper-bundle.css11'></link>
 
 
