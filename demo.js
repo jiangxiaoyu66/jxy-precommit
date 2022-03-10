522392483604c680fcf606a6d04f542506e8929e
SQB <sqb@beifendeMacBook-Pro.local>
2022-03-10 09:54:46 +0800

    demo

--- demo.js
+++ demo.js
@@ -1,7 +1,4 @@
 //  const cdn = 'https://statics.cdn.com/cdn/swiper/8.0.6/swiper-bundle.css'
  <link rel="stylesheet" href='https://statics.cdn.com/cdn/swiper/8.0.6/swiper-bundle.css11'></link>
- <link rel="stylesheet" href='https://statics.cdn.com/cdn/swiper/8.0.6/swiper-bundle.css11'></link>
-
-
 
 
