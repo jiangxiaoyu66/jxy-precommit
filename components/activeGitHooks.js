a8c09d14572b94c492839d8a816fa1deaf6c03a8
SQB <sqb@beifendeMacBook-Pro.local>
2022-05-11 15:52:31 +0800

    feature: feature: 只在mac系统下执行chmod赋予权限命令

--- components/activeGitHooks.js
+++ components/activeGitHooks.js
@@ -2,16 +2,20 @@
  * 激活githooks使其生效
  *  */ 
 function activeGitHooks() {
-  const exec = require('child_process').exec
+  if(process.platform === 'darwin') {  // unix系统，即苹果
+    const exec = require('child_process').exec
 
-  exec('chmod 700 .git/hooks/*', 'utf8', (err, stdout, stderr) => {
-    if (err) {
-      console.log('err:', err)
-      console.log('stderr:', stderr)
-    } else {
-      console.log(`\x1b[32m 已激活githooks \x1b[0m`);
-    }
-  })
+    exec('chmod 700 .git/hooks/*', 'utf8', (err, stdout, stderr) => {
+      if (err) {
+        console.log('err:', err)
+        console.log('stderr:', stderr)
+      } else {
+        console.log(`\x1b[32m 已激活githooks \x1b[0m`);
+      }
+    })
+  }
+
+ 
 }
 
 module.exports = activeGitHooks
